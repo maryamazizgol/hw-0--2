6c3cff2d29f205f75fb638aebbfe44130e3c9c45
maryam <mrymaz2179@gmail.com>
2020-01-29 12:10:08 +0330

    hw(0)_2

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number=15;
+
+        if(number%2==0){
+            System.out.println("zoj ");
+        }else{
+            System.out.println("fard");
+        }
+    }
+}
+}
